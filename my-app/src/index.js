c187799949cacfa22a7358f7ffed579a1dd84da2
littlemad <littlemad@gmail.com>
2020-03-17 16:29:51 +0100

    Sass working

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/basic.css';
+import './css/basic.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
