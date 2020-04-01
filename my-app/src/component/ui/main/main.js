4c468b0beeaadc3838e45f1f9b70e4c2b233c396
littlemad <littlemad@gmail.com>
2020-04-01 10:52:08 +0200

    changes

--- my-app/src/component/ui/main/main.js
+++ my-app/src/component/ui/main/main.js
@@ -1,87 +1,113 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import infoData from '../../../infoData';
 
 const state = {
-    name: 'Giovanni',
-    age: 30,
+  name: 'Giovanni',
+  age: 30,
 };
 
 function handleClick(e) {
-console.log(e.target);
+  console.log(e.target);
 }
 
 function mouseOver(e) {
-console.log(e.target);
+  console.log(e.target);
 }
 
 function handleCopy(e) {
-console.log('be original!!!!!!');
+  console.log('be original!!!!!!');
 }
 
-function ContactCard(props){
+function ContactCard(props) {
+  /* Example on how to use dynamic multiple classNames */
   return (
     <div className={`item${props.contact.imgUrl ? ' img' : ''}`}>
       <div className={!props.contact.imgUrl ? 'item' : 'item img'}>
-        <div style={{border: props.contact.imgUrl && "10px solid #f00"}}>
-            <img alt="" src={props.contact.imgUrl}/>
-            <h3>{props.contact.name}</h3>
-            <p>Phone: {props.contact.phone}</p>
-            <p>Email: {props.contact.email}</p>
-        </div>
+        <div style={{ border: props.contact.imgUrl && '10px solid #f00' }}>
+          <img alt="" src={props.contact.imgUrl} />
+          <h3>{props.contact.name}</h3>
+          <p>Phone: {props.contact.phone}</p>
+          <p>Email: {props.contact.email}</p>
         </div>
       </div>
-      )
+    </div>
+  );
 }
 
 function MyTag(props) {
   return (
-      <div>
-          <h3>Question: {props.question}</h3>
-          <h3>Answer: {props.punchLine}</h3>
-          <hr/>
-      </div>
-  )
+    <div>
+      <h3>Question: {props.question}</h3>
+      <h3>Answer: {props.punchLine}</h3>
+      <hr />
+    </div>
+  );
 }
 
+function StateSet(props) {
+  const myInitialState = 'active';
+  const [myState, mySetState] = useState(myInitialState);
+  return (
+    <div>
+      <button onClick={() => mySetState({active: false})}>
+        buttoooooooooooooon {myState}
+      </button>
+    </div>
+  );
+}
 
 function main() {
-    const infoComponent = infoData.map(info =>  <MyTag question={info.question} punchLine={info.punchLine} />)
+  /* How to create a component from a data and pass variables */
+  const infoComponent = infoData.map(info => (
+    <MyTag key={info.id} question={info.question} punchLine={info.punchLine} />
+  ));
 
-    return (
-        <header className="app-header">
-        <p>
-          Name is {state.name} and my age is {state.age}
-        </p>
-        <button onClick={handleClick}>Click test</button>
-        <br />
-        <br />
-        <button onMouseOver={mouseOver}>Mouse over</button>
-        <br />
-        <br />
-        <p onCopy={handleCopy}>Miao bau don't copy</p>
+  return (
+    <header className="app-header">
+      <p>
+        Name is {state.name} and my age is {state.age}
+      </p>
+      <button onClick={handleClick}>Click test</button>
+      <br />
+      <br />
+      <button onMouseOver={mouseOver}>Mouse over</button>
+      <br />
+      <br />
+      <p onCopy={handleCopy}>Miao bau don't copy</p>
 
-        <br />
-        <br />
-        <ContactCard 
-              contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
-        />
+      <br />
+      <br />
+      <ContactCard
+        contact={{
+          name: 'Mr. Whiskerson',
+          imgUrl: 'http://placekitten.com/300/200',
+          phone: '(212) 555-1234',
+          email: 'mr.whiskaz@catnap.meow',
+        }}
+      />
 
-        <ContactCard 
-              contact={{name: "Mr. Whiskerson", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
-        />
+      <ContactCard
+        contact={{
+          name: 'Mr. Whiskerson',
+          phone: '(212) 555-1234',
+          email: 'mr.whiskaz@catnap.meow',
+        }}
+      />
 
-        <div>
-          Hello {state.name + " space " + state.age} more text
-        </div>
+      <div>Hello {state.name + ' space ' + state.age} more text</div>
 
-        <br />
-        <br />
+      <br />
+      <br />
 
-        {infoComponent}
+      {infoComponent}
 
-        </header>
-    )
+      <br />
+      <br />
+
+      <StateSet />
+    </header>
+  );
 }
 
 export default main;
